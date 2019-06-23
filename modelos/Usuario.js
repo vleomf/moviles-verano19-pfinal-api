b5037943889e87e9ff4274249bd49a9bfb4f99aa
Leopoldo Muñoz <viceo00@gmail.com>
2019-06-22 21:44:39 -0500

    Eliminando comentarios erroneos, eran de una deficion que ya no existe

--- modelos/Usuario.js
+++ modelos/Usuario.js
@@ -1,24 +1,13 @@
 /// DEFINICION DE MODELO USUARIO.
 
 /// LOS PARAMETROS CORRESPONDEN AL CONSTRUCTOR.
-/// SE PUEDEN ASIGNAR VALORES POR DEFAULT
-/// PARA QUE EL CONSTRUCTOR SEA FLEXIBLE.
-/// EJEMPLO:
-
-///     matricula         -> ES UN PARAMETRO REQUERIDO
-///     apPaterno         -> ES UN PARAMETRO REQUERIDO
-///     apMaterno         -> ES UN PARAMETRO REQUERIDO
-///     nombre            -> ES UN PARAMETRO REQUERIDO
-///     correoElectronico -> ES UN PARAMETRO REQUERIDO
-///     fotografia        -> ES UN PARAMETRO CON VALOR POR DEFAULT (ES POSIBLE NO INCLUIRLO EN EL CONSTRUCTOR)
-///     rol               -> ES UN PARAMETRO CON VALOR POR DEFAULT (EL ROL POR DEFAULT SERA `estudiante`)
 
 /// A PARTIR DE ES6, EXISTE LA PALABRA RESERVADA CLASS 
 /// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 
 class Usuario
 {
-    constructor(matricula, apPaterno, apMaterno, nombre, correoElectronico, fotografia = null, rol = 'estudiante')
+    constructor(matricula, apPaterno, apMaterno, nombre, correoElectronico, fotografia, rol)
     {
         /// INICIALIZAMOS ATRIBUTOS
         this.matricula = matricula;
